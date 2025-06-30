8a9074828c03f8eceb687c934fd25183443a6b73
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2025-06-30 20:59:14 +0900

    macOS 호환성 추가

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+
+# 현재 디렉토리를 Python 경로에 추가
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 """
 파일 자동 분류 프로그램
 메인 진입점
